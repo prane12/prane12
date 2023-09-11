b94e1340358abce2c57ec04e628bae4682e4a7f5
prane12 <pranesh9096@gmail.com>
2023-09-11 21:47:33 +0530

    first commmit for docker

---
+++ src/main/java/com/spdoc/docker123/Docker123Application.java
@@ -0,0 +1,13 @@
+package com.spdoc.docker123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Docker123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Docker123Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spdoc/docker123/Docker123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spdoc.docker123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Docker123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
